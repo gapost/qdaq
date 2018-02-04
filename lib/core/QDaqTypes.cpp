38e92a6102565ad90b12a190ebbe21c60ff759b0
George Apostolopoulos <gapost@ipta.demokritos.gr>
2018-02-04 18:51:28 +0200

    Implemented explicitly shared QDaqBuffer.

--- lib/core/QDaqTypes.cpp
+++ lib/core/QDaqTypes.cpp
@@ -34,3 +34,4 @@ int registerVectorTypes(QScriptEngine* eng)
 
 
 
+
--- lib/core/QDaqTypes.h
+++ lib/core/QDaqTypes.h
@@ -1,9 +1,10 @@
-#ifndef _RTTYPES_H_
-#define _RTTYPES_H_
+#ifndef _QDAQTYPES_H_
+#define _QDAQTYPES_H_
 
 #include "QDaqGlobal.h"
 
 #include <QVector>
+#include <QExplicitlySharedDataPointer>
 #include <QMetaType>
 
 typedef QVector<int> QDaqIntVector;
@@ -74,11 +75,10 @@ public:
         QTime T = QDateTime(*this).time();
         return T.toString("hh:mm:ss.zzz");
     }
-
 };
 
 template<class T>
-class QDaqBufferImpl
+class buffer : public QSharedData
 {
 public:
     enum StorageType { Open, Fixed, Circular };
@@ -86,10 +86,10 @@ public:
     typedef QVector<T> container_t;
 
 private:
-    typedef QDaqBufferImpl<T> _Self;
+    typedef buffer<T> _Self;
 
     /// memory buffer
-    container_t buffer;
+    container_t mem;
     /// vector size
     int sz;
     /// vector capacity
@@ -105,14 +105,23 @@ private:
 
     void normalize_()
     {
-        if (type_==Circular && sz!=0 && sz==cp && tail>0)
+        if (type_==Circular && sz && sz==cp && tail)
         {
-            QVector<T> temp(sz);
-            const T* head = buffer.data() + tail;
-            int sz1 = sz-tail;
-            memcpy(temp.data(), head, sz1*sizeof(T));
-            memcpy(temp.data()+sz1, buffer.data(), (sz-sz1)*sizeof(T));
-            memcpy(buffer.data(), temp.data(), sz*sizeof(T));
+            int m = sz-tail;
+            T* head = mem.data();
+            T* temp = head + sz;
+            if (tail<=sz/2)
+            {
+                memcpy(temp,head,tail*sizeof(T));
+                memmove(head,head+tail,m*sizeof(T));
+                memcpy(head + m,temp,tail*sizeof(T));
+            }
+            else
+            {
+                memcpy(temp,head+tail,m*sizeof(T));
+                memmove(head+m,head,tail*sizeof(T));
+                memcpy(head,temp,m*sizeof(T));
+            }
             tail = 0;
         }
     }
@@ -124,7 +133,7 @@ private:
 
     void set_(int i, const T& v)
     {
-        buffer[i] = v;
+        mem[i] = v;
     }
 
     void calcBounds_()
@@ -145,23 +154,23 @@ private:
     }
 
 public:
-    explicit QDaqBufferImpl(quint32 acap = 0) : buffer((int)acap),
+    explicit buffer(int acap = 0) : mem((int)acap),
         sz(0), cp(acap), type_(Fixed), tail(0),
         x1(0), x2(0), recalcBounds(true)
     {
     }
-    QDaqBufferImpl(const _Self& rhs) : buffer(rhs.buffer),
+    buffer(const _Self& rhs) : mem(rhs.mem),
         sz(rhs.sz), cp(rhs.cp), type_(rhs.type_), tail(rhs.tail),
         x1(rhs.x1), x2(rhs.x2), recalcBounds(rhs.recalcBounds)
     {
     }
-    ~QDaqBufferImpl(void)
+    ~buffer(void)
     {
     }
 
     _Self& operator=(const _Self& rhs)
     {
-        buffer = rhs.buffer;
+        mem = rhs.mem;
         sz = rhs.sz;
         cp = rhs.cp;
         type_ = rhs.type_;
@@ -172,25 +181,41 @@ public:
         return (*this);
     }
 
-    StorageType type() const { return type_; }
-    void setType(StorageType newt) { if (newt!=type_){ normalize_(); type_ = newt; } }
-    int capacity() const { return (int)cp; }
     int size() const { return (int)sz; }
+
+    StorageType type() const { return type_; }
+    void setType(StorageType newt)
+    {
+        if (newt==type_) return;
+
+        normalize_();
+
+        if (newt==Circular)
+            mem.resize(cp + cp/2);
+        if (type_==Circular)
+            mem.resize(cp);
+
+        type_ = newt;
+    }
+
+    int capacity() const { return (int)cp; }
 
     void setCapacity(int c)
     {
         if (c==cp) return;
 
         normalize_();
-        buffer.resize(c);
+
         switch (type_)
         {
         case Fixed:
         case Open:
+            mem.resize(c);
             if (sz>c) sz = c;
             recalcBounds = true;
             break;
         case Circular:
+            mem.resize(c + c/2);
             if (c>cp)
             {
                 if (sz==cp) tail = sz;
@@ -216,15 +241,15 @@ public:
     void replace(const container_t& other)
     {
         clear();
-        buffer = other;
-        sz = buffer.size();
-        cp = buffer.capacity();
+        mem = other;
+        sz = mem.size();
+        cp = mem.capacity();
+        if (type_==Circular) mem.resize(cp  + cp/2);
         tail = 0;
     }
-
     const T& get(int i) const
     {
-        return buffer[idx_(i)];
+        return mem[idx_(i)];
     }
     const T& operator[](int i) const
     {
@@ -232,11 +257,10 @@ public:
     }
     void push(const T& v)
     {
-        recalcBounds = true;
         switch (type_)
         {
         case Open:
-            if (sz==cp) buffer.resize(++cp);
+            if (sz==cp) mem.resize(++cp);
             set_(sz++,v);
             break;
         case Fixed:
@@ -248,6 +272,48 @@ public:
             if (sz==cp) tail %= sz;
             break;
         }
+        recalcBounds = true;
+    }
+    void push(const T* v, int n)
+    {
+        int m;
+        switch (type_)
+        {
+        case Open:
+            if (sz+n>cp) { cp = sz+n; mem.resize(cp); }
+            memcpy(mem.data()+sz,v,n*sizeof(T));
+            sz += n;
+            break;
+        case Fixed:
+            m = n;
+            if (sz+n>cp) m = cp - sz;
+            if (m) {
+                memcpy(mem.data()+sz,v,m*sizeof(T));
+                sz += m;
+            }
+            break;
+        case Circular:
+            if (n>=cp) {
+                memcpy(mem.data(),v+n-cp,cp*sizeof(T));
+                tail = 0;
+                sz = cp;
+            }
+            else if (n<=cp-tail) {
+                memcpy(mem.data()+tail,v,n*sizeof(T));
+                tail += n;
+                if (sz<cp) sz += n;
+                if (sz==cp) tail %= cp;
+            } else {
+                m = cp-tail;
+                memcpy(mem.data()+tail,v,m*sizeof(T));
+                v += m; n -= m;
+                memcpy(mem.data(),v,n*sizeof(T));
+                tail = n;
+                sz = cp;
+            }
+            break;
+        }
+        recalcBounds = true;
     }
     _Self& operator<<(const T& v)
     {
@@ -256,12 +322,12 @@ public:
     const T* constData() const
     {
         const_cast< _Self * >( this )->normalize_();
-        return buffer.constData();
+        return mem.constData();
     }
     container_t vector() const
     {
         const_cast< _Self * >( this )->normalize_();
-        return buffer.mid(0,sz);
+        return mem.mid(0,sz);
     }
     double vmin() const
     {
@@ -298,9 +364,52 @@ public:
     }
 };
 
-typedef QDaqBufferImpl<double> QDaqBuffer;
+class QDaqBuffer
+{
+    typedef buffer<double> buffer_t;
+    QExplicitlySharedDataPointer<buffer_t> d_ptr;
+public:
+    enum StorageType {
+        Open = buffer_t::Open, Fixed, Circular
+    };
 
-Q_DECLARE_METATYPE(QDaqBuffer*)
+    explicit QDaqBuffer(int cap = 0)
+    {
+        d_ptr = new buffer_t(cap);
+    }
+    QDaqBuffer(const QDaqBuffer& other) : d_ptr(other.d_ptr)
+    {}
+    QDaqBuffer& operator=(const QDaqBuffer& rhs)
+    {
+        d_ptr = rhs.d_ptr;
+        return (*this);
+    }
+    int size() const { return d_ptr->size(); }
+    StorageType type() const { return (StorageType)(d_ptr->type()); }
+    void setType(StorageType newt) {
+        d_ptr->setType((buffer_t::StorageType)newt);
+    }
+    int capacity() const { return d_ptr->capacity(); }
+    void setCapacity(int c) { d_ptr->setCapacity(c); }
+    void clear() { d_ptr->clear(); }
+    void replace(const QDaqDoubleVector& v) { d_ptr->replace(v); }
+    double get(int i) const { return d_ptr->get(i); }
+    double operator[](int i) const { return d_ptr->get(i); }
+    void push(double v) { d_ptr->push(v); }
+    void push(const double* v, int n) { d_ptr->push(v, n); }
+    QDaqBuffer& operator<<(const double& v)
+    {
+        d_ptr->push(v); return (*this);
+    }
+    const double* constData() const { return d_ptr->constData(); }
+    QDaqDoubleVector toVector() const { return d_ptr->vector(); }
+    double vmin() const { return d_ptr->vmin(); }
+    double vmax() const { return d_ptr->vmax(); }
+    double mean() const { return d_ptr->mean(); }
+    double std() const { return d_ptr->std(); }
+};
+
+Q_DECLARE_METATYPE(QDaqBuffer)
 
 #endif
 
