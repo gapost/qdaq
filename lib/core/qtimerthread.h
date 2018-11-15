206f1e3aafad3e93172a2cc9419ae67bc6c6077b
George Apostolopoulos <gapost@ipta.demokritos.gr>
2018-11-15 13:38:19 +0200

    Documented qtimerthread class.

--- lib/core/qtimerthread.h
+++ lib/core/qtimerthread.h
@@ -5,17 +5,33 @@
 #include <QThread>
 #include <QTimer>
 
-/** timer thread
-  * A timer thread implemented using timer_create() + signal
-  * + a thread that watches for the signal
-  */
+/**
+ * @brief A timer thread class.
+ *
+ * QTimerThread inheritis QThread and uses internally QTimer to implement
+ * its functionality.
+ *
+ * After calling start() a thread is spawned and the function timer_func()
+ * is called periodically with period equal to the interval property (in ms).
+ *
+ * To use the class one has to subclass QTimerThread reimplement timer_func(). Note
+ * that timer_func() is executed from the sepatrate timer thread thus and the user
+ * is responsible for synchronization issues.
+ *
+ * The accuracy of the timer depends on Qt's QTimer class. According to their docs
+ * QTimer uses the best OS timing routines in each case.
+ *
+ * @ingroup Core
+ */
 
 class QTimerThread : public QThread
 {
     Q_OBJECT
 
-    QTimer timer_;
+    /** Timer repetition interval in ms. */
+    Q_PROPERTY(int interval READ interval WRITE setInterval)
 
+    QTimer timer_;
 
 protected slots:
     virtual void timer_func() {}
