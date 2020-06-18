047b71fd6fae33bf7bca6d74c5ac072f230d98d7
George Apostolopoulos <gapost@ipta.demokritos.gr>
2020-06-18 15:30:49 +0300

    qdaq/main.cpp: do not run empty script in console mode

--- qdaq/main.cpp
+++ qdaq/main.cpp
@@ -128,13 +128,13 @@ int main(int argc, char *argv[])
         QDaqConsole* console = new QDaqConsole(s);
         console->show();
 
+        console->stdOut("QDaq - Qt-based Data Aqcuisition\n");
+        console->stdOut(QString("Version %1\n").arg(QDaq::Version()));
 
-        console->stdOut("QDaq - Qt-based Data Aqcuisition");
-        console->stdOut(QString("Version %1\n\n\n").arg(QDaq::Version()));
-
-//        s->evaluate(QString("print('Executing startup script %1')").arg(startupScript));
-        s->evaluate(QString("log('Executing startup script %1')").arg(startupScript));
-        s->evaluate(QString("exec('%1')").arg(startupScript));
+        if (!startupScript.isEmpty()) {
+            s->evaluate(QString("log('Executing startup script %1')").arg(startupScript));
+            s->evaluate(QString("exec('%1')").arg(startupScript));
+        }
 
 
         if (s->getEngine()->hasUncaughtException())
