3c34fb66577597abbc27675addd9c46fe9af3796
Panos Stamoulis <pstamoul@phys.uoa.gr>
2020-06-24 14:58:33 +0300

    Return to status quo ante

--- test/scripts/testWidgets.js
+++ test/scripts/testWidgets.js
@@ -22,15 +22,6 @@ from = w.findChild('plotWdgt');
 
 w.replaceWidget(from,to);
 
-to = loadUi('ui/buttonform.ui');
-from = w.findChild('buttCtrl');
-
-w.replaceWidget(from,to);
-
-to = loadUi('ui/resmeas.ui');
-from = w.findChild('resmeasCtrl');
-
-w.replaceWidget(from,to);
 
 log("Creating Loop");
 
@@ -102,50 +93,6 @@ function onPlotCh1(on) {
 }
 
 
-function TabAdd() {
-    var index = w.findChild('sBindex').value;
-    var name = w.findChild('cBnames').currentText;
-    log("Here goes something " + index + " -- " + name );
-    var session = qdaq.session0;
-    log("loaded: " + name);
-//    var mainf = w.findChild("plotForm");
-//    session.insertTab(index,mainf,'cpPlot');
-    session.insertTab(index,name,'cpPlot');
-}
-
-function TabRem() {
-    var index = w.findChild('sBindex').value;
-    log("Here goes nothing "+index);
-    var session = qdaq.session0;
-    session.deleteTab(index);
-}
-
-function ChanAdd() {
-    var Box = w.findChild("groupBox");
-    var R = w.findChild("R1");
-    var Layout = w.findChild("gridLayout_2");
-
-    var number = w.findChild('sCindex').value;
-    var session = qdaq.session0;
-    session.insertWidget(Layout,R,"whatever");
-    //for (i =0-->number){
-    //      R+i = QLineEdit
-    //      R+i label  = QLabel
-    //      Layout.addWidget(R+i)
-    //      Layout.addWidget(R+i label)
-    //      addTab
-    //          TabLayout.addWidget
-    //}
-    log("In Channel Add "+number);
-
-}
-
-function ChanRem() {
-    var index = w.findChild('sCindex').value;
-    log("In Channel Rem "+index);
-
-}
-
 
 btPlotCh1 = w.findChild('btPlotCh1');
 btPlotCh1.toggled.connect(onPlotCh1);
@@ -153,15 +100,6 @@ btPlotCh1.toggled.connect(onPlotCh1);
 btClear = w.findChild('btClear');
 btClear.clicked.connect(Data.clear);
 
-btTabAdd = w.findChild('btTabAdd');
-btTabAdd.clicked.connect(TabAdd);
-btTabRem = w.findChild('btTabRem');
-btTabRem.clicked.connect(TabRem);
-
-btTabAdd = w.findChild('btChanAdd');
-btTabAdd.clicked.connect(ChanAdd);
-btTabRem = w.findChild('btChanRem');
-btTabRem.clicked.connect(ChanRem);
 
 loop2.arm()
 w.show()
