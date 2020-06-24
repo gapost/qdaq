c3bcf6096b150b99d004170f3a8e914fcbc73359
Panos Stamoulis <pstamoul@phys.uoa.gr>
2020-06-24 16:15:36 +0300

    Aesthetics

--- test/scripts/testresmeasChan.js
+++ test/scripts/testresmeasChan.js
@@ -10,7 +10,6 @@ function ChanAdd() {
     // -User has to know the name of the tabWidget they want to modify
     var tabWidg = w.findChild("tabWidget1");
 
-
     var session = qdaq.session0;
 
     // Find out number of samples already on ui
@@ -18,43 +17,41 @@ function ChanAdd() {
     while (Boolean(w.findChild('R'+(nSamples+1)))) {
         nSamples++;
     }
-        //construct object names and text
-        var R = loadUi('ui/lonechannel.ui');
-        var labelWi = R.findChild("label1")
-        var chanWi = R.findChild("R1");
+
+    //construct object names and text
+    var R = loadUi('ui/lonechannel.ui');
+    var chanWi = R.findChild("R1");
+    var labelWi = R.findChild("label1")
 
 
-        var chname = "R" + (nSamples+1);
-        var lblname = "label" + (nSamples+1);
+    var chname = "R" + (nSamples+1);
+    var lblname = "label" + (nSamples+1);
+    var lbltext = "Ch" + (nSamples+1);
     // The user also sets the widget name for the inserted widget
     //(so that it can be deleted later)
-        var extrname = "whatever" + (nSamples+1);
+    var extrname = "whatever" + (nSamples+1);
 
-        var lbltext = "Ch" + (nSamples+1);
+    session.rename(chanWi,chname);
+    session.rename(R,extrname);
+    session.rename(labelWi,lblname);
+    labelWi.setText(lbltext);
 
-        session.rename(chanWi,chname);
+    var newtab = loadUi('ui/lonetab.ui')
+    var HwChanWi = newtab.findChild("HwChan1");
+    var CurSourWi = newtab.findChild("CurSour1");
+    var cbPlotWi = newtab.findChild("cbPlt1");
+    var CurSourname = "CurSour" + (nSamples+1);
+    var HwChname = "HwChan" + (nSamples+1);
+    var cbPlname = "cbPlt" + (nSamples+1);
+    var tabtext = "Sample " + (nSamples+1);
 
-        session.rename(R,extrname);
+    session.rename(CurSourWi,CurSourname);
+    session.rename(HwChanWi,HwChname);
+    session.rename(cbPlotWi,cbPlname);
 
-        session.rename(labelWi,lblname);
-        labelWi.setText(lbltext);
-
-        var newtab = loadUi('ui/lonetab.ui')
-        var HwChanWi = newtab.findChild("HwChan1");
-        var CurSourWi = newtab.findChild("CurSour1");
-        var cbPlotWi = newtab.findChild("cbPlt1");
-        var CurSourname = "CurSour" + (nSamples+1);
-        var HwChname = "HwChan" + (nSamples+1);
-        var cbPlname = "cbPlt" + (nSamples+1);
-        var tabtext = "Sample " + (nSamples+1);
-
-        session.rename(HwChanWi,HwChname);
-        session.rename(CurSourWi,CurSourname);
-        session.rename(cbPlotWi,cbPlname);
-
-        //put things in place
-        session.insertWidget(Box,R);
-        session.insertTab(nSamples,newtab,tabtext,tabWidg);
+    //put things in place
+    session.insertWidget(Box,R);
+    session.insertTab(nSamples,newtab,tabtext,tabWidg);
 }
 
 function ChanRem() {
@@ -80,7 +77,6 @@ function ChanRem() {
     session.deleteWidget(w,lblrem);
     session.deleteWidget(w,extrrem);
     session.deleteTab(nSamples-1,tabWidg);
-
 }
 
 
