aa89ede52ac73dbe2b42e2a7e0b2b36bf5da9f9b
George Apostolopoulos <gapost@ipta.demokritos.gr>
2020-10-19 15:33:56 +0300

    Modified makeArchive.sh to include submodules in the tar file

--- makeArchive.sh
+++ makeArchive.sh
@@ -1,10 +1,49 @@
-#!/bin/sh
+#!/bin/bash
+#
+# 
+# Submodule stuff from Khaja Minhajuddin
+# File name: makeArchive.sh
+# cd qdaq-root-folder; ./makeArchive.sh
+
 gitversion=`git describe --always --tags  | sed -e 's/-/./g' | sed -e 's/g//g'`
 prefix="qdaq-"$gitversion"/"
 filename="../qdaq-"$gitversion
-git archive --format=tar --prefix=$prefix HEAD > $filename".tar"
+ROOT_ARCHIVE_DIR="$(pwd)"
+
+echo "> create root archive"
+git archive --format=tar --prefix=$prefix --output $ROOT_ARCHIVE_DIR"/repo-output.tar" HEAD 
+
+echo "> add gitversion.txt"
 mkdir "qdaq-"$gitversion
 echo "$gitversion" > "qdaq-"$gitversion"/"gitversion.txt
-tar rf $filename".tar" "qdaq-"$gitversion"/"gitversion.txt
-gzip $filename".tar"
+tar rf $ROOT_ARCHIVE_DIR"/repo-output.tar" "qdaq-"$gitversion"/"gitversion.txt
 rm -r "qdaq-"$gitversion
+
+echo "> append submodule archives"
+pushd gui/qconsolewidget
+git archive --format=tar --prefix=$prefix"/gui/qconsolewidget/" --output $ROOT_ARCHIVE_DIR"/repo-output-sub1.tar" HEAD
+popd
+tar --concatenate --file repo-output.tar repo-output-sub1.tar
+pushd gui/qmatplotwidget
+git archive --format=tar --prefix=$prefix"/gui/qmatplotwidget/" --output $ROOT_ARCHIVE_DIR"/repo-output-sub2.tar" HEAD
+popd
+tar --concatenate --file repo-output.tar repo-output-sub2.tar
+echo "> remove all sub tars"
+rm -rf repo-output-sub*.tar
+
+echo "> gzip final tar"
+gzip --force --verbose repo-output.tar
+
+echo "> move output file"
+mv repo-output.tar.gz $filename".tar.gz"
+
+echo "> done"
+
+
+
+
+
+
+
+
+
