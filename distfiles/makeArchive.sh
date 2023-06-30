be7b0df9c97dc859d9a70be70e778cca1d5ec494
George Apostolopoulos <gapost@ipta.demokritos.gr>
2023-06-30 12:13:21 +0300

    updated pkg build for win and linux

--- distfiles/makeArchive.sh
+++ distfiles/makeArchive.sh
@@ -20,14 +20,22 @@ tar rf $ROOT_ARCHIVE_DIR"/repo-output.tar" "qdaq-"$gitversion"/"gitversion.txt
 rm -r "qdaq-"$gitversion
 
 echo "> append submodule archives"
+
 pushd src/gui/qconsolewidget
 git archive --format=tar --prefix=$prefix"src/gui/qconsolewidget/" --output $ROOT_ARCHIVE_DIR"/repo-output-sub1.tar" HEAD
 popd
 tar --concatenate --file repo-output.tar repo-output-sub1.tar
+
 pushd src/gui/qmatplotwidget
 git archive --format=tar --prefix=$prefix"src/gui/qmatplotwidget/" --output $ROOT_ARCHIVE_DIR"/repo-output-sub2.tar" HEAD
 popd
 tar --concatenate --file repo-output.tar repo-output-sub2.tar
+
+pushd src/core/qthdf5
+git archive --format=tar --prefix=$prefix"src/core/qthdf5/" --output $ROOT_ARCHIVE_DIR"/repo-output-sub3.tar" HEAD
+popd
+tar --concatenate --file repo-output.tar repo-output-sub3.tar
+
 echo "> remove all sub tars"
 rm -rf repo-output-sub*.tar
 
